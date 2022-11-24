b12e97f0c5732fb31a1a84ce2e3e23635d16e80e
Forrest Chew <forrestchew@gmail.com>
2022-11-23 21:34:15 -0700

    refactored for readibility

--- test/NftFactory.js
+++ test/NftFactory.js
@@ -1,27 +1,27 @@
-const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
-const { expect } = require("chai");
-const contractFixtures = require("./fixtures");
+const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
+const { deployNftFactoryFixture } = require('./fixtures');
+const { expect } = require('chai');
 
-describe("NftFactory", () => {
-  describe("Deployment", () => {
-    it("Deploys NftFactory smart contract", async () => {
-      const { nftFactory } = await loadFixture(contractFixtures);
+describe('NftFactory', () => {
+  describe('Deployment', () => {
+    it('Deploys NftFactory smart contract', async () => {
+      const { nftFactory } = await loadFixture(deployNftFactoryFixture);
       expect(nftFactory.address.length).to.equal(42);
     });
-    it("Sets the correct Nft collection name", async () => {
-      const { nftFactory } = await loadFixture(contractFixtures);
-      expect(await nftFactory.name()).to.equal("TestToken");
+    it('Sets the correct Nft collection name', async () => {
+      const { nftFactory } = await loadFixture(deployNftFactoryFixture);
+      expect(await nftFactory.name()).to.equal('TestToken');
     });
-    it("Sets the correct Nft collection symbol", async () => {
-      const { nftFactory } = await loadFixture(contractFixtures);
-      expect(await nftFactory.symbol()).to.equal("TT");
+    it('Sets the correct Nft collection symbol', async () => {
+      const { nftFactory } = await loadFixture(deployNftFactoryFixture);
+      expect(await nftFactory.symbol()).to.equal('TT');
     });
   });
-  describe("Minting", () => {
-    it("Creates collectable", async () => {
-      const tokenUri = "https://testtokenuri.uri";
+  describe('Minting', () => {
+    it('Creates collectable', async () => {
+      const tokenUri = 'https://testtokenuri.uri';
       const { nftFactory, nftFactoryDeployer } = await loadFixture(
-        contractFixtures
+        deployNftFactoryFixture
       );
       await nftFactory.createCollectable(tokenUri);
       expect(await nftFactory.balanceOf(nftFactoryDeployer.address)).to.equal(
@@ -29,11 +29,11 @@ describe("NftFactory", () => {
       );
       expect(await nftFactory.tokenURI(1)).to.equal(tokenUri);
     });
-    it("Creates multiple collectables", async () => {
+    it('Creates multiple collectables', async () => {
       let nftId = 1;
       const tokenUri = `https://testtokenuri.uri_${nftId}`;
       const { nftFactory, nftFactoryDeployer } = await loadFixture(
-        contractFixtures
+        deployNftFactoryFixture
       );
       for (let i = 0; i < 5; i++) {
         await nftFactory.createCollectable(tokenUri);
