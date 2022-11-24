d6cbc4cea2d16478d89838eddf030b53d15acb54
Forrest Chew <forrestchew@gmail.com>
2022-11-23 21:32:09 -0700

    Testing doubly linked list stack structure for storing nft listings in auction

--- test/DLLStack.js
+++ test/DLLStack.js
@@ -1,43 +1,44 @@
-const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
-const { ethers } = require("hardhat");
-const { expect } = require("chai");
-const contractFixtures = require("./fixtures");
+const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
+const { ethers } = require('hardhat');
+const { expect } = require('chai');
+const { createSingleNftAndApproveAuction } = require('./fixtures');
 
-describe("Create stack item", () => {
+describe('DoublyLinkedListStack', () => {
+  const listingFee = ethers.utils.parseEther('0.1');
   let startingPrice;
   let nftLister;
   let nftFactAddr;
   let nftAuctionAddr;
   let currentNft;
   beforeEach(async () => {
-    const { nftAuction, listingFee, nftFactory } = await loadFixture(
-      contractFixtures
+    const { nftAuction, nftFactory, randAccount_1 } = await loadFixture(
+      createSingleNftAndApproveAuction
     );
     nftAuctionAddr = nftAuction.address;
     nftFactAddr = nftFactory.address;
-    nftLister = await ethers.getSigner();
-    const tokenUri = "https://testtokenuri.uri";
-    await nftFactory.createCollectable(tokenUri);
+    nftLister = randAccount_1;
+    startingPrice = ethers.utils.parseEther('1');
     const tokenId = 1;
-    startingPrice = ethers.utils.parseEther("1");
-    await nftAuction.listNft(nftFactory.address, tokenId, startingPrice, {
-      value: listingFee,
-    });
+    await nftAuction
+      .connect(randAccount_1)
+      .listNft(nftFactory.address, tokenId, startingPrice, {
+        value: listingFee,
+      });
     currentNft = await nftAuction.getCurrentNft();
   });
-  it("Pushes item onto stack", async () => {
+  it('Pushes item onto stack', async () => {
     expect(currentNft.length).to.equal(4);
   });
-  it("Sets node nftListing", async () => {
+  it('Sets node nftListing', async () => {
     expect(currentNft[0].length).to.equal(6);
   });
-  it("Sets node key", async () => {
+  it('Sets node key', async () => {
     expect(currentNft[1].length).to.equal(66);
   });
-  it("Sets node next", async () => {
+  it('Sets node next', async () => {
     expect(currentNft[2].length).to.equal(66);
   });
-  it("Sets node prev", async () => {
+  it('Sets node prev', async () => {
     expect(currentNft[3].length).to.equal(66);
   });
 });
